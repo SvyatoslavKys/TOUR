f5eaf88683b473f29b5d7626118f461b59e8fe41
SvyatoslavKys <158611727+SvyatoslavKys@users.noreply.github.com>
2024-11-14 14:27:47 +0100

    added section: camping and about_us
    added section: camping and about_us

---
+++ js/menuOpen.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('[data-menu-button]');
+    const closeButton = document.querySelector('[data-close-menu]');
+    const menu = document.querySelector('[data-menu]');
+
+   
+    menuButton.addEventListener('click', () => {
+        menuButton.classList.add('is-open');
+        menu.classList.remove('is-hiden');
+    });
+
+   
+    closeButton.addEventListener('click', () => {
+        menuButton.classList.remove('is-open');
+        menu.classList.add('is-hiden');
+    });
+});
