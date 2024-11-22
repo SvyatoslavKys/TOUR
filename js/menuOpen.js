6c60419da85a4754c0f1c24c7aa841f77e414139
SvyatoslavKys <158611727+SvyatoslavKys@users.noreply.github.com>
2024-11-22 20:40:50 +0100

    ok

--- js/menuOpen.js
+++ js/menuOpen.js
@@ -2,20 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuButton = document.querySelector('[data-menu-button]');
     const closeButton = document.querySelector('[data-close-menu]');
     const menu = document.querySelector('[data-menu]');
-    const menuButtonAbout = document.querySelector('[data-menu-button-about]');
-    const closeButtonAbout = document.querySelector('[data-close-menu-about]');
-    const menuAbout = document.querySelector('[data-menu-about]');
 
-    menuButtonAbout.addEventListener('click', () => {
-        menuButtonAbout.classList.add('is-open');
-        menuAbout.classList.remove('is-hiden');
-    });
-
-    closeButtonAbout.addEventListener('click', () => {
-        menuButtonAbout.classList.remove('is-open');
-        menuAbout.classList.add('is-hiden');
-    });
-   
+    
     menuButton.addEventListener('click', () => {
         menuButton.classList.add('is-open');
         menu.classList.remove('is-hiden');
