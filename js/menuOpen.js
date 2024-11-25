7099c17b439664b1058085c571c976db85f7df6a
SvyatoslavKys <158611727+SvyatoslavKys@users.noreply.github.com>
2024-11-25 19:58:19 +0100

    Optimized HTML/SCSS
    Optimized HTML/SCSS

--- js/menuOpen.js
+++ js/menuOpen.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Selectors
     const menuButton = document.querySelector('[data-menu-button]');
     const closeButton = document.querySelector('[data-close-menu]');
     const menu = document.querySelector('[data-menu]');
@@ -6,24 +7,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeButtonAbout = document.querySelector('[data-close-menu-about]');
     const menuAbout = document.querySelector('[data-menu-about]');
 
-    menuButtonAbout.addEventListener('click', () => {
-        menuButtonAbout.classList.add('is-open');
-        menuAbout.classList.remove('is-hiden');
-    });
+    // Helper function to toggle menus
+    function toggleMenu(button, menu, isOpenClass, isHiddenClass, shouldOpen) {
+        if (button && menu) {
+            if (shouldOpen) {
+                button.classList.add(isOpenClass);
+                menu.classList.remove(isHiddenClass);
+            } else {
+                button.classList.remove(isOpenClass);
+                menu.classList.add(isHiddenClass);
+            }
+        }
+    }
 
-    closeButtonAbout.addEventListener('click', () => {
-        menuButtonAbout.classList.remove('is-open');
-        menuAbout.classList.add('is-hiden');
-    });
-   
-    menuButton.addEventListener('click', () => {
-        menuButton.classList.add('is-open');
-        menu.classList.remove('is-hiden');
-    });
+    // Menu About event listeners
+    if (menuButtonAbout && closeButtonAbout && menuAbout) {
+        menuButtonAbout.addEventListener('click', () => {
+            toggleMenu(menuButtonAbout, menuAbout, 'is-open', 'is-hiden', true);
+        });
 
-   
-    closeButton.addEventListener('click', () => {
-        menuButton.classList.remove('is-open');
-        menu.classList.add('is-hiden');
-    });
+        closeButtonAbout.addEventListener('click', () => {
+            toggleMenu(menuButtonAbout, menuAbout, 'is-open', 'is-hiden', false);
+        });
+    }
+
+    // Main Menu event listeners
+    if (menuButton && closeButton && menu) {
+        menuButton.addEventListener('click', () => {
+            toggleMenu(menuButton, menu, 'is-open', 'is-hiden', true);
+        });
+
+        closeButton.addEventListener('click', () => {
+            toggleMenu(menuButton, menu, 'is-open', 'is-hiden', false);
+        });
+    }
 });
